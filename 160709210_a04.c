0f74261d9519a6eff1a55cecb200dd8b29fce66e
vinusiva <vinu.siva@hotmail.com>
2020-08-01 08:58:17 -0600

    ‘message’

--- 160709210_a04.c
+++ 160709210_a04.c
@@ -1,5 +1,29 @@
 /*Vinuyaa Sivanathan
 Cp386*/
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <time.h>
+
+typedef struct process //represents a single thread, you can add more members if required
+{
+	int max;
+} Process;
+
+
+int main(int argc, char *argv[])
+{
+
+	Process* processes = NULL;
+
+	printf("Number of Customers:5\n");
+	printf("Currently Available resources:%s %s %s %s\n",argv[1],argv[2],argv[3],argv[4]);
+
+	return 0;
+}
 
 
